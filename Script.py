5d36eab5b1eee118d2a523cfa24e5eb58ef8f627
Adroaldo J. Pagliari <adroaldo@outlook.com>
2017-09-13 14:20:09 -0300

    py script updated

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[22]:
+# In[3]:
 
 
 import csv
@@ -9,39 +9,39 @@ f = open('guns.csv', 'r')
 data = list(csv.reader(f))
 
 
-# In[23]:
+# In[4]:
 
 
 print(data[:5])
 
 
-# In[24]:
+# In[5]:
 
 
 headers = data[:1]
 headers
 
 
-# In[27]:
+# In[6]:
 
 
 data = data[1:]
 
 
-# In[28]:
+# In[7]:
 
 
 data[:5]
 
 
-# In[31]:
+# In[8]:
 
 
 years = [x[1] for x in data]
 years
 
 
-# In[32]:
+# In[9]:
 
 
 year_counts = {}
@@ -55,20 +55,20 @@ for year in years:
 year_counts
 
 
-# In[40]:
+# In[10]:
 
 
 import datetime
 dates = [datetime.datetime(year=int(x[1]),month=int(x[2]),day=1) for x in data]
 
 
-# In[42]:
+# In[11]:
 
 
 dates[0:4]
 
 
-# In[44]:
+# In[12]:
 
 
 
@@ -82,7 +82,7 @@ for date in dates:
 date_counts
 
 
-# In[46]:
+# In[13]:
 
 
 sex_counts = {}
@@ -94,7 +94,7 @@ for row in data:
 sex_counts
 
 
-# In[47]:
+# In[14]:
 
 
 race_counts = {}
@@ -108,7 +108,7 @@ race_counts
 
 # Data facts: The most part of gun deaths involve men of white race. The minorities are'nt so affect by these possible crimes, but we need to verify the intents of crimes. Another data must be considered further: season.
 
-# In[53]:
+# In[15]:
 
 
 with open('census.csv', 'r') as f:
@@ -116,13 +116,13 @@ with open('census.csv', 'r') as f:
 census
 
 
-# In[54]:
+# In[16]:
 
 
 race_counts
 
 
-# In[55]:
+# In[17]:
 
 
 mapping = {
@@ -134,7 +134,7 @@ mapping = {
 }
 
 
-# In[56]:
+# In[18]:
 
 
 race_per_hundredk = {}
@@ -144,21 +144,21 @@ for k,v in race_counts.items():
 race_per_hundredk
 
 
-# In[57]:
+# In[19]:
 
 
 intents = [x[3] for x in data]
 intents
 
 
-# In[58]:
+# In[20]:
 
 
 races = [x[7] for x in data]
 races
 
 
-# In[60]:
+# In[21]:
 
 
 homicide_race_counts = {}
@@ -172,7 +172,7 @@ homicide_race_counts
     
 
 
-# In[61]:
+# In[22]:
 
 
 race_per_hundredk = {}
@@ -185,3 +185,30 @@ race_per_hundredk
 # Despite of the most part of guns deaths was associated to white men, when we only consider homicides its evident that there is a correlation among black people and these deaths, the same for hispanics.
 # 
 # In future we need to verify if there's a correlation between other intents and races and between other variables with races and intents.
+
+# In[23]:
+
+
+months = [x[2] for x in data]
+
+homicide_month_counts = {}
+for i,month in enumerate(months):
+    if intents[i] == 'Homicide':
+        if month not in homicide_month_counts:
+            homicide_month_counts[month] = 1
+        else:
+            homicide_month_counts[month] += 1
+homicide_month_counts
+
+
+# In[37]:
+
+
+total_homicides = sum(homicide_month_counts.values())#35176
+
+
+# In[38]:
+
+
+total_homicides
+
